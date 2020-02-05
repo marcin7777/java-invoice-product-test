265e9a8fa5e3cb9a51091f2f069ea1571dfd3abc
marcin7777 <marcinmz9@tlen.pl>
2020-02-05 22:35:51 +0100

    First change in Product class that is about price in  class constructor

---
+++ src/main/java/Quiz.java
@@ -0,0 +1,9 @@
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		String _______SDFlkasdjhflkjashfkasdfl = "ala";
+		System.out.println("ala" == "ala");
+	}
+
+}
---
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -0,0 +1,39 @@
+package pl.edu.agh.mwo.invoice.product;
+
+import java.math.BigDecimal;
+
+public abstract class Product {
+	private final String name;
+
+	private final BigDecimal price;
+
+	private final BigDecimal taxPercent;
+
+	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name == null || name.equals("")) {
+			throw new IllegalArgumentException("Product cannot be null");
+		} 
+		if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalArgumentException("Product cannot be null");
+		}
+		this.name = name;
+		this.price = price;
+		this.taxPercent = tax;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public BigDecimal getPrice() {
+		return this.price;
+	}
+
+	public BigDecimal getTaxPercent() {
+		return this.taxPercent;
+	}
+
+	public BigDecimal getPriceWithTax() {
+		return this.price.add(this.taxPercent.multiply(this.price));
+	}
+}
